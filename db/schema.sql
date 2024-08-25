47190b73bb68ddfc532fe25c141fbed436dbe4d4
Wrandy1 <cjyoung0420@gmail.com>
2024-08-25 14:23:09 -0500

    started db and index.js

---
+++ db/schema.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+CREATE TABLE role (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department INTEGER NOT NULL,
+    FOREIGN KEY (department)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+
+
+CREATE TABLE employee (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL
+);
---
+++ index.js
@@ -0,0 +1,33 @@
+const inquirer = require('inquirer');
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'postgres',
+  password: 'Jetthedog1!',
+  host: 'localhost',
+  database: 'employee_db'
+},
+console.log('Connected to employee_db')
+);
+
+pool.connect();
+
+const questions = [
+  {
+      type: 'list',
+      name: 'input',
+      message: "What would you like to do?",
+      choices: ['View all departments',
+          'View all roles',
+          'Add a role',
+          'View all employees',
+          'Add an employee',
+          'Update an employee role',
+          'View All Departments',
+          'Add a department',
+          
+          'Exit'
+        ],
+  },
+
+];
